c18ed48dfaae4b2e9e935b132abff2a25167bdfd
Jason Xu <Xinwen31415@gmail.com>
2021-09-21 21:13:25 -0500

    modified a litte

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,16 @@
-import bodyParser from "body-parser";
-import express from "express";
+import express from 'express';
 
 const app = express();
 const port = process.env.PORT || 3333;
 
-app.use(bodyParser.json());
-app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
-app.use(bodyParser.text({ type: "text/html" }));
+app.use(express.json());
+app.use(express.raw({ type: 'application/vnd.custom-type' }));
+app.use(express.text({ type: 'text/html' }));
 
-app.get("/", async (req, res) => {
-  res.json({ Hello: "World" });
+app.get('/', async (req, res) => {
+	res.json({ Hello: 'World' });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+	console.log(`Example app listening at http://localhost:${port}`);
 });
